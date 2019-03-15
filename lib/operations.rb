4f6afe105c1435cb006cabf367f5524321fc50d9
Matthew Eric <mf9646a@alumni.american.edu>
2019-03-15 18:07:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-  speed < 60
-
+  if speed > 60 || speed < 40
+    return true
+  else
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? true : false
+	speed > 60 || speed < 40 ? return true : return false
 end
 	
 
